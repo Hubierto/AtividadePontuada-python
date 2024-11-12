f2ed7d1237f2da0ad3c97e48a6ba8dcc272276ec
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-11-12 13:42:11 +0000

    ok

--- projeto/models/juridica.py
+++ projeto/models/juridica.py
@@ -2,12 +2,41 @@ from abc import ABC
 from projeto.models.endereco import Endereco
 from projeto.models.pessoa import Pessoa
 
-class juridica(Pessoa, ABC):
+class Juridica(Pessoa, ABC):
 
-    def __init__(self, id: int, nome: str, telefone: str, email: str, endereco: Endereco, cnpj: str, inscricaoEstadual: str) -> None:
+    def __init__(self, id: int, nome: str, telefone: str, email: str, endereco: Endereco, cnpj: int, inscricaoEstadual: str) -> None:
         super().__init__(id, nome, telefone, email, endereco)
-        self.cnpj = cnpj
-        self.inscricaoEstadual = inscricaoEstadual
+        self.cnpj = self._verificar_cnpj(cnpj)
+        self.inscricaoEstadual = self._verificar_inscricaoEstadual(inscricaoEstadual)
+
+    def _verificar_cnpj(self, valor: int) -> int:
+        self._verificar_cnpj_tipo_invalido(valor)
+        self._verificar_cnpj_negativo(valor)
+        self._verificar_cnpj_vazio(valor)
+
+    def _verificar_inscricaoEstadual(self, valor: int) -> int:
+        self._verificar_inscricaoEstadual_tipo_invalido(valor)    
+        self._verificar_inscricaoEstadual_vazio(valor)    
+
+    def _verificar_cnpj_tipo_invalido(self, valor):
+        if not isinstance(valor, int):
+            raise TypeError("CNPJ inválido!")
+
+    def _verificar_cnpj_negativo(self, valor):
+        if valor <= 0:
+            raise ValueError("CNPJ deve ser positivo!")
+
+    def _verificar_cnpj_vazio(self, valor):
+        if not valor.strip():
+            raise TypeError("CNPJ não pode ser vazio!")
+
+    def _verificar_inscricaoEstadual_tipo_invalido(self, valor):
+        if not isinstance(valor, int):
+            raise TypeError("Inscrição Estadual inválido!")
+
+    def _verificar_inscricaoEstadual_vazio(self, valor):
+        if not valor.strip():
+            raise TypeError("Incrição Estadual não pode ser vazio!")
 
     def __str__(self) -> str:
         return( 
---
+++ projeto/tests/test_juridica.py
@@ -0,0 +1,17 @@
+import pytest
+from models.juridica import Juridica
+from projeto.models.endereco import Endereco
+from projeto.models.enums.unidadefederativa import UnidadeFederativa
+
+@pytest.fixture
+def juridica_valido():
+    juridica1 = Juridica(12345, "Rosa", "88998899", "rosa@gmail.com", Endereco, 99999, "001")
+    return juridica1
+
+def test_cnpj_valido(juridica_valido):
+    assert juridica_valido.cnpj == 12345
+
+def test_nome_valido(juridica_valido):
+    assert juridica_valido.nome == "Rosa"
+
+ 
